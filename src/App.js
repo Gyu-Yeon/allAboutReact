e91c728cb35c8b10128d9b1eb74f0b939c8d64fc
Gyu-Yeon <idsopy@nate.com>
2022-06-23 00:03:29 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyComponent from "./MyComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MyComponent>리엑트</MyComponent>;
 }
 
 export default App;
+
+//삼항연산자 && 사용할 때, 주의점: React는 false를 렌더링할 때는 null과 마찬가지로 아무것도 나타나지 않지만,
+// falsy한 값인 0은 예외적으로 화면에 나타난다. const name = 0; return <div>{name && <h1>리엑트입니다.</h1>}</div>;
